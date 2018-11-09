fdc5b15bd841e692b0f8be924d2ed8330616f3b7
Jake Thwaites <jakethwaites@hotmail.co.uk>
2018-11-09 14:40:55 +0000

    created index pages

---
+++ controllers/animals_controller.rb
@@ -0,0 +1,12 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative( '../models/animal.rb' )
+require_relative( '../models/owner.rb' )
+require_relative( '../models/adoption.rb' )
+also_reload( '../models/*' )
+
+get '/animals' do
+  @animals = Animal.all()
+  erb ( :"animals/index" )
+end
