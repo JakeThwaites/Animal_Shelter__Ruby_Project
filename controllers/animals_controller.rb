923d0ed55a353095ce80cc80f12b619bf15c3219
Jake Thwaites <jakethwaites@hotmail.co.uk>
2018-11-09 15:01:08 +0000

    created erb files for animals

--- controllers/animals_controller.rb
+++ controllers/animals_controller.rb
@@ -10,3 +10,31 @@ get '/animals' do
   @animals = Animal.all()
   erb ( :"animals/index" )
 end
+
+get '/animals/new' do
+  erb(:"animals/new")
+end
+
+post '/animals' do
+  animal = Animal.new(params)
+  animal.save()
+  redirect to("/animals")
+end
+
+get '/animals/:id' do
+  erb(:"/animals/show")
+end
+
+get '/animals/:id/edit' do
+  erb(:"/animals/edit")
+end
+
+post '/animals/:id' do
+  animal = animal.update()
+  redirect to("/animals/show")
+end
+
+post '/animals/:id/delete' do
+  animal.delete()
+  redirect to(:"/animals")
+end
